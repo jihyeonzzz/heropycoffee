8c0a3e1e629441f9cd89ab857b7e5f59c2e82908
jihyeonzzz <112531703+jihyeonzzz@users.noreply.github.com>
2022-10-07 15:38:23 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,14 +1,24 @@
 // 292.p
 const badgeEl = document.querySelector('.badges');
+const topBtnEl = document.querySelector('.top_btn');
+
+topBtnEl.addEventListener('click', function(){
+  // alert('eiownviowne');
+  gsap.to(window, .6, { //브라우저를 선택하여 0.6초만에
+    scrollTop:0 //스크롤 꼭때기 0까지 올라간다.
+  });
+});
 
 window.addEventListener('scroll', function(){
   console.log(window.scrollY); //스크롤값 확인하기
-  if(760<this.window.scrollY){
+  if(700<this.window.scrollY){
     // badgeEl.style.display='none';
-    gsap.to(badgeEl, 0.6,{opacity:0, display:'none'})
+    gsap.to(badgeEl, 0.6,{opacity:0, display:'none'});
+    gsap.to(topBtnEl, 0.6,{opacity:1});
   }else{
     // badgeEl.style.display='block';
-    gsap.to(badgeEl, 0.6,{opacity:1, display:'block'})
+    gsap.to(badgeEl, 0.6,{opacity:1, display:'block'});
+    gsap.to(topBtnEl, 0.6,{opacity:0});
   }
 });
 
@@ -39,3 +49,21 @@ toggleBtnEl.addEventListener('click', function(){
     promotionEl.classList.add('hide');
   }
 });
+
+
+// 6, 7, 8, 10 콘텐츠가 화면의 80% 지점에 보일 때 동작하는 스크립트
+const spyEl = document.querySelectorAll('section.scroll-spy');
+
+spyEl.forEach(function(spyEl){
+  new ScrollMagic
+  .Scene({ //감시할 장면을 추가
+    triggerElement:spyEl,
+    triggerHook:0.8 //화면의 80% 지점에서 동작
+  })
+  .setClassToggle(spyEl, 'show') //show클래스를 적용하거나 해제하기
+  .addTo(new ScrollMagic.Controller()); //컨트롤러에 장면을 할당한다.
+});
+
+// 푸터 카피라이트
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
